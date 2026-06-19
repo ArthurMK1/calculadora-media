cbc5f866d9c8faedb637afab84e67fdc233c98ec
Arthur Luís <arthur.luismk@gmail.com>
2026-06-19 19:40:36 +0000

    Relatório de ALuno

--- projetoaula2.py
+++ projetoaula2.py
@@ -1,13 +1,34 @@
 #VERSIONAMENTO git
-nota1 = float(input("Nota 1: "))
-nota2 = float(input("Nota 2: "))
-nota3 = float(input("Nota 3: "))
+alunos = []
+notas = []
+medias = []
+while True:
+    aluno = input("Qual o nome do Aluno: ")
+    alunos.append(aluno)
 
-media = (nota1 + nota2 + nota3) / 3
+    nota1 = float(input("Nota 1: "))
+    nota2 = float(input("Nota 2: "))
+    nota3 = float(input("Nota 3: "))
+    notas.append([nota1, nota2, nota3])
 
-if media >= 7:
-    print("ALUNO APROVADO!!!")
-else: 
-    print("O aluno foi reprovado")
+    media = (nota1 + nota2 + nota3) / 3
+    medias.append(media)
 
-print(media)
+    op = input("Você deseja continuar? (s/n) ")
+    if (op == "n"):
+        break
+    
+print("\n====RELATORIO====")
+for i in range(len(alunos)):
+    print(f"\nAluno(a): {alunos[i]}")
+    print(f"As notas do(a) aluno(a) foram: {notas[i][0]}, {notas[i][1]}, {notas[i][2]}")
+    print(f"E a média do(a) aluno(a) foi: {medias[i]:.2f}")
+
+    if medias[i] >= 7:
+        status = "Aprovado"
+    elif medias[i] >= 5: 
+        status = "Recuperação"
+    else:
+        status = "Reprovado"
+
+    print(f"STATUS: {status}")
